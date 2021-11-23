6820c9f0a2ab609d9d2517e726da111eebffc632
bilocq <bilocqet@gmail.com>
2021-11-23 18:39:22 -0500

    A few more comments in ttc.py

--- ttc.py
+++ ttc.py
@@ -3,8 +3,8 @@ Cleaned up code for training Trust The Critics (TTC) scheme. Will train a list o
 dataset to the target dataset. 
 
 IMPORTANT INPUTS
-    - source (defaults to noise): A dataset which the gradients of the critics will push towards the target dataset.
-    - target (defaults to mnist): Another dataset.
+    - source (defaults to noise): The name of the distribution or dataset to push towards the target.
+    - target (defaults to mnist): The name of a dataset.
     - data (required): A directory where both datasets are located.
     - temp_dir (required): A directory where the trained critics will be saved.
     - model: The choice of architectures for the critics.
